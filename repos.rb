f984fe9da6cfe8953f8caba4894f8c5b02218e35
codcod <nikos@linux.com>
2025-06-15 07:36:23 +0200

    Repos v1.2.0

--- repos.rb
+++ repos.rb
@@ -2,7 +2,7 @@ class Repos < Formula
   desc "Clone multiple GitHub repositories and run arbitrary commands inside them."
   homepage "https://github.com/codcod/repos"
   url "https://github.com/codcod/repos/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "ad5d89439d7e50aac82de3fd1c2ce8c90d9eae4c1a955677783eeced987cd55c"
+  sha256 "4ae3b3db40381cfac9a9d095de60d928abf465dd3bac2add3e1ea5c3d0887b49"
   license "MIT" # or whatever license you use
 
   depends_on "go" => :build
