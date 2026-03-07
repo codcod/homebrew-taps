dbc9ca89699a863467e2f64d72a2d10c6b31354d
codcod <nikos@linux.com>
2026-03-07 17:24:40 +0100

    fix: typo

--- maints-triage.rb
+++ maints-triage.rb
@@ -1,5 +1,5 @@
-class MainsTriage < Formula
-  desc "CLI tool for triaging and managing GitHub repository maintenance issues."
+class MaintsTriage < Formula
+  desc "CLI tool for triaging and managing maintenance issues."
   homepage "https://github.com/codcod/maints-triage"
   url "https://github.com/codcod/maints-triage/releases/download/v0.1.0/triage-v0.1.0-darwin-universal.tar.gz"
   sha256 "0f26d93b77666f14de55b3f2a8235f3a6f4a4b694afa36d965ed55358ff3f76e"
