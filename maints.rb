464cbc42bca696395bbcf927446483ceaec621af
codcod <nikos@linux.com>
2026-04-26 13:47:56 +0200

    refactor: maints-triage became maints

---
+++ maints.rb
@@ -0,0 +1,15 @@
+class Maints < Formula
+  desc "CLI tool for triaging and managing maintenance issues."
+  homepage "https://github.com/codcod/maints"
+  url "https://github.com/codcod/maints/releases/download/v0.5.0/triage-v0.5.0-darwin-universal.tar.gz"
+  sha256 "539a73c86aa2f41656f86092cc77931e4f8bb5cc4fc2b1b581d72b065217fca5"
+  license "MIT"
+
+  def install
+    bin.install "triage" => "maints"
+  end
+
+  test do
+    system "bin/maints", "--version"
+  end
+end
